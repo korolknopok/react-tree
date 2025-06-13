c493d4db9734d81f096633f6a041c826e1397aec
Назмутдинов Артём <94155787+korolknopok@users.noreply.github.com>
2025-06-13 13:20:30 +0700

    feature(task7-add-child-to-node): add opportunity to add child to node.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,52 @@
 import './App.css';
 import {Tree} from "./components/Tree/Tree.tsx";
-import {treeData} from "./data/treeData.ts";
+import {type TreeData, treeData} from "./data/treeData.ts";
+import {useState} from "react";
+import {useNodeStore} from "./useNodeStore.ts";
 
-const App = () => {
+export const App = () => {
+    const [tree, setTree] = useState<TreeData[]>(JSON.parse(JSON.stringify(treeData)));
+    const [counter, setCounter] = useState<number>(5);
+
+    const addChildToNode = (parentId: number | null, newNode: TreeData) => {
+        const updatedTree = JSON.parse(JSON.stringify(tree));
+        const walkAndAdd = (nodes: TreeData[]) => {
+            if (parentId === null) {
+                nodes.push(newNode);
+                return;
+            }
+            for (let node of nodes) {
+                if (node.id === parentId) {
+                    if (!node.children)
+                        node.children = [];
+                    node.children.push(newNode);
+                    return;
+                }
+                if (node.children) walkAndAdd(node.children);
+            }
+        };
+        walkAndAdd(updatedTree);
+        setTree(updatedTree);
+    }
+
+    const handleAdd = () => {
+        const newNode: TreeData = {
+            id: counter,
+            name: `Node ${counter}`,
+        };
+        addChildToNode(useNodeStore.getState().selectedNodeId, newNode);
+        setCounter(counter + 1);
+    }
+
     return (
         <div className="app">
             <div className="container">
                 <h2>Tree</h2>
-                    <Tree data={treeData}/>
+                <Tree data={tree}/>
+                <div className="buttons">
+                    <button onClick={handleAdd}> Add</button>
+                </div>
             </div>
         </div>
     );
 };
-
-export default App;
